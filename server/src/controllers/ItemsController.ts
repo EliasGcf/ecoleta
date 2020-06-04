ced302a58787abb68f647329cd043016d3bd75ea
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-06-04 11:53:55 -0300

    feat(mobile): finish the app mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -5,15 +5,15 @@ import knex from '../database/connection';
 class ItemsController {
   async index (req: Request, res: Response): Promise<Response> {
     const items = await knex('items').select('*');
-  
+
     const serializedItems = items.map(item => {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.123:3333/uploads/${item.image}`,
       }
     })
-  
+
     return res.json(serializedItems);
   }
 }
