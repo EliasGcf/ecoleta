e9eeb136e1b307c8259b4701d3b4f39f08b2badc
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-06-05 09:52:42 -0300

    chore(server): add validation to create points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 
@@ -17,6 +18,22 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.string().required(),
+      longitude: Joi.string().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+      items: Joi.string().required(),
+    }),
+  }, { abortEarly: false }),
+  pointsController.create
+);
 
 export default routes;
